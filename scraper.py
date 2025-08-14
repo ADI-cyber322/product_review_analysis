20e7ba938208a66d6c2d34de8054226a42dc6ff2
ADI-cyber322 <adityabhatia293@gmail.com>
2025-08-14 17:32:59 +0530

    Create scraper.py

---
+++ scraper.py
@@ -0,0 +1,11 @@
+
+
+def get_amazon_reviews(product_name):
+  
+    return ["Great product!", "Not worth the price", "Excellent features"]
+
+def get_flipkart_reviews(product_name):
+    return ["Value for money", "Battery lasts long", "Good build quality"]
+
+def get_ebay_reviews(product_name):
+    return ["Works well", "Fast shipping", "Too expensive"]
